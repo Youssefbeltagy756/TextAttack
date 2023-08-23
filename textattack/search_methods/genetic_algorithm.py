3b887eeedbcfc040b502d6486b882e2b895e4c07
Youssefbeltagy756 <89200918+Youssefbeltagy756@users.noreply.github.com>
2023-08-23 07:52:30 +0300

    Update genetic_algorithm.py

--- textattack/search_methods/genetic_algorithm.py
+++ textattack/search_methods/genetic_algorithm.py
@@ -327,7 +327,7 @@ class GeneticAlgorithm(PopulationBasedSearch, ABC):
                     break
 
             population = [population[0]] + children
-        
+        population = sorted(population, key=lambda x: x.result.score, reverse=True)
         return population[0].result
 
     def check_transformation_compatibility(self, transformation):
