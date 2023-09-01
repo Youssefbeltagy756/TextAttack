184d31dcf435d4e004274571c1816b88adb756a7
Youssefbeltagy756 <89200918+Youssefbeltagy756@users.noreply.github.com>
2023-09-01 19:00:44 +0300

    Update genetic_algorithm.py

--- textattack/search_methods/genetic_algorithm.py
+++ textattack/search_methods/genetic_algorithm.py
@@ -284,7 +284,7 @@ class GeneticAlgorithm(PopulationBasedSearch, ABC):
 
     def perform_search(self, initial_result, array_modifiable_indeces):
         self._search_over = False
-        population = self._initialize_population(initial_result, self.pop_size)
+        population = self._initialize_population(initial_result, self.pop_size, array_modifiable_indeces)
         pop_size = len(population)
         current_score = initial_result.score
     
