b4695a5cf2c2ee31aaa2dd7f26d9245f945e9b52
Youssefbeltagy756 <89200918+Youssefbeltagy756@users.noreply.github.com>
2023-09-05 15:41:58 +0300

    Update genetic_algorithm.py

--- textattack/search_methods/genetic_algorithm.py
+++ textattack/search_methods/genetic_algorithm.py
@@ -319,11 +319,7 @@ class GeneticAlgorithm(PopulationBasedSearch, ABC):
     
         population.append(best_member)  
         population = sorted(population, key=lambda x: x.result.score, reverse=True)
-        if best_member.result == population[0].result:
-            print("perturbation mattered")
-        else:
-            print("perturbation didn't matter")
-        return population[0].result
+        return best_member.result
 
     ########################################################################################################
     ########################################################################################################
